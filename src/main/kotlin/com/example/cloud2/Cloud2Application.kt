9b10d4350c794dee2de9d040135d8b8fd0853efc
Rick Bijkerk <rick.bijkerk@luminis.eu>
2018-11-15 16:35:47 +0100

    first commit

---
+++ src/main/kotlin/com/example/cloud2/Cloud2Application.kt
@@ -0,0 +1,11 @@
+package com.example.cloud2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Cloud2Application
+
+fun main(args: Array<String>) {
+    runApplication<Cloud2Application>(*args)
+}
---
+++ src/main/kotlin/com/example/cloud2/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.example.cloud2
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder) : SpringApplicationBuilder {
+		return application.sources(Cloud2Application::class.java)
+	}
+
+}
